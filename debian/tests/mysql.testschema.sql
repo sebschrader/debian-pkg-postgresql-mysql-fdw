7217489b2aeb8d933ca4a285d7da0426844681f5
Christoph Berg <myon@debian.org>
2016-01-22 21:34:55 +0100

    debian/tests: Add new table and disable unshare.

--- debian/tests/mysql.testschema.sql
+++ debian/tests/mysql.testschema.sql
@@ -4,13 +4,7 @@ USE testdb;
 CREATE USER 'foo'@'127.0.0.1' IDENTIFIED BY 'bar';
 GRANT ALL PRIVILEGES ON testdb.* TO 'foo'@'127.0.0.1';
 
-CREATE TABLE department(
-  department_id INT AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(20)
-);
-
-CREATE TABLE employee(
-  emp_id INT AUTO_INCREMENT PRIMARY KEY,
-  emp_name VARCHAR(20),
-  emp_dept_id INT REFERENCES department(department_id)
-);
+-- from mysql_init.sh
+CREATE TABLE department(department_id int, department_name text, PRIMARY KEY (department_id));
+CREATE TABLE employee(emp_id int, emp_name text, emp_dept_id int, PRIMARY KEY (emp_id));
+CREATE TABLE empdata (emp_id int, emp_dat blob, PRIMARY KEY (emp_id));
